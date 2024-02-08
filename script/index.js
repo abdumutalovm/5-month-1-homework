29444781b6bee09004e87d4154706cea4ed8079c
abdumutalovm <mirziyodabdumutalov83@gmail.com>
2024-02-09 00:45:12 +0500

    5-month 1-homework

---
+++ script/index.js
@@ -0,0 +1,28 @@
+// function age(age) {
+//   let age_ = +prompt("Iltimos yoshingizni kiriting");
+
+//   if (!Number(age_)) {
+//     console.log("Iltimos raqam bilan kiriting");
+//   } else if (age_ >= 100) {
+//     console.log("Bu inson uchun juda katta yosh");
+//   } else if (age_ <= 5) {
+//     console.log("Siz hali juda kichiksiz");
+//   } else {
+//     console.log(age_);
+//   }
+// }
+// console.log(age());
+
+// function pNumber(Pnumber) {
+//   let number = +prompt(
+//     "Iltimos telefon raqamingizni kiriting",
+//     "+998911234567"
+//   );
+
+//   if (number > 999999999999 || number < 99999999999) {
+//     console.log("Telefon raqam kamida 13ta belgidan iborat bolishi kerak");
+//   } else {
+//     console.log("+" + number);
+//   }
+// }
+// console.log(pNumber());
